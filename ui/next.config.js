4d806a99841781d2b469964fd7bfeb3a9100bca5
ratnakar dashrath yadav <117161937+ratn7921@users.noreply.github.com>
2025-06-20 11:54:00 +0530

    Update next.config.js

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,28 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-//   async rewrites() {
-//     return [
-//       {
-//         source: '/api/upload',
-//         destination: 'http://localhost:8080/upload',
-//       },
-//       {
-//         source: '/api/download/:port',
-//         destination: 'http://localhost:8080/download/:port',
-//       },
-//     ];
-//   },
-// }
-
-// module.exports = nextConfig
-
-
-
-
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -30,7 +5,7 @@ const nextConfig = {
     const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
     if (!API_URL) {
-      throw new Error("Environment variable NEXT_PUBLIC_API_URL is not defined.");
+      throw new Error("NEXT_PUBLIC_API_URL is not defined");
     }
 
     return [
@@ -47,4 +22,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
