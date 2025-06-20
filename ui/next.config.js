71a420f4ca713db6189293a3f0aa037d6b635694
ratnakar dashrath yadav <117161937+ratn7921@users.noreply.github.com>
2025-06-20 14:31:08 +0530

    Update next.config.js

--- ui/next.config.js
+++ ui/next.config.js
@@ -3,11 +3,9 @@ const nextConfig = {
   swcMinify: true,
   async rewrites() {
     const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
     if (!API_URL) {
       throw new Error("NEXT_PUBLIC_API_URL is not defined");
     }
-
     return [
       {
         source: '/api/upload',
